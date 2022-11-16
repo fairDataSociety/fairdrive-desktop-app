1af23c8005c2e38695fcacb586cd5c64d5d4da47
asabya <asabya@gmail.com>
2022-11-16 17:27:34 +0530

    update menu

--- app/main.go
+++ app/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"context"
 	"embed"
+	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/datafund/fdfs/pkg/handler"
 	"github.com/fairdatasociety/fairOS-dfs/pkg/logging"
-	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/application"
+	"github.com/wailsapp/wails/v2/pkg/menu"
+	"github.com/wailsapp/wails/v2/pkg/menu/keys"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
@@ -23,7 +27,7 @@ func main() {
 	}
 	cnf := &conf{}
 	// Create application with options
-	err = wails.Run(&options.App{
+	app := application.NewWithOptions(&options.App{
 		Title:  "app",
 		Width:  1024,
 		Height: 768,
@@ -53,8 +57,20 @@ func main() {
 			dfsHandler.Close()
 		},
 	})
-
-	if err != nil {
+	appMenu := menu.NewMenu()
+	fileMenu := appMenu.AddSubmenu("File")
+	fileMenu.AddText("Preferences", keys.CmdOrCtrl(","), func(_ *menu.CallbackData) {
+		fmt.Println("Preferences")
+	})
+	fileMenu.AddSeparator()
+	fileMenu.AddText("Quit", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
+		app.Quit()
+	})
+	if runtime.GOOS == "darwin" {
+		appMenu.Append(menu.EditMenu()) // on macos platform, we should append EditMenu to enable Cmd+C,Cmd+V,Cmd+Z... shortcut
+	}
+	app.SetApplicationMenu(appMenu)
+	if err := app.Run(); err != nil {
 		println("Error:", err.Error())
 	}
 }
