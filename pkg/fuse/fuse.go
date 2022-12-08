76b1ef9d62d1bc956159932f1e821524530c96a0
asabya <asabya@gmail.com>
2022-12-08 11:00:52 +0530

    fix: #85

--- pkg/fuse/fuse.go
+++ pkg/fuse/fuse.go
@@ -156,6 +156,7 @@ func New(sessionId string, pod *pod.Info, api *api.DfsAPI, logger logging.Logger
 func (f *Ffdfs) Statfs(_ string, stat *fuse.Statfs_t) int {
 	// TODO fix space availability logic based on batchID
 	// bFree is just a place holder for now for demo
+	stat.Frsize = sysBlockSize
 	stat.Bsize = sysBlockSize
 	stat.Bfree = free / sysBlockSize
 	stat.Bavail = free / sysBlockSize
